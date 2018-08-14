c23faf8f84820d1e2dc3378eba4883b553be84cb
Saugata Dutta <saugata.dutta@gmail.com>
2018-08-14 17:33:44 +0530

    QR code generator code.

--- src/main/java/com/falabella/controller/CodeGeneratorController.java
+++ src/main/java/com/falabella/controller/CodeGeneratorController.java
@@ -2,91 +2,70 @@ package com.falabella.controller;
 
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
 import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageConfig;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.google.zxing.common.HybridBinarizer;
+import org.apache.commons.io.FileUtils;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageOutputStream;
-import javax.imageio.stream.MemoryCacheImageOutputStream;
-import java.awt.Graphics2D;
-import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.util.Hashtable;
 
 @RestController
 @RequestMapping("/api/v1/code")
 public class CodeGeneratorController {
 
-    private static BufferedImage getQRImage(String qrCodeText, int size)
-            throws WriterException, IOException {
-        // Create the ByteMatrix for the QR-Code that encodes the given String
-        Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<>();
-        hintMap.put(EncodeHintType.ERROR_CORRECTION,
-                ErrorCorrectionLevel.L);
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix byteMatrix = qrCodeWriter.encode(qrCodeText,
+    @GetMapping(value = "encode", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] createQRCode(@RequestParam String text) throws WriterException, IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        BitMatrix matrix = new MultiFormatWriter().encode(text,
                 BarcodeFormat.QR_CODE,
-                size,
-                size,
-                hintMap);
-        // Make the BufferedImage that are to hold the QRCode
-        int matrixWidth = byteMatrix.getWidth();
-        BufferedImage image = new BufferedImage(matrixWidth,
-                matrixWidth,
-                BufferedImage.TYPE_INT_RGB);
-        image.createGraphics();
-
-        Graphics2D graphics = (Graphics2D) image.getGraphics();
-        graphics.setColor(Color.WHITE);
-        graphics.fillRect(0,
-                0,
-                matrixWidth,
-                matrixWidth);
-        // Paint and save the image using the ByteMatrix
-        graphics.setColor(Color.BLACK);
-
-        for (int i = 0; i < matrixWidth; i++) {
-            for (int j = 0; j < matrixWidth; j++) {
-                if (byteMatrix.get(i,
-                        j)) {
-                    graphics.fillRect(i,
-                            j,
-                            1,
-                            1);
-                }
-            }
-        }
-        return image;
+                200,
+                200);
+        MatrixToImageWriter.writeToStream(matrix,
+                MediaType.IMAGE_PNG.getSubtype(),
+                baos,
+                new MatrixToImageConfig());
+        return baos.toByteArray();
     }
 
-    @GetMapping(produces = MediaType.IMAGE_GIF_VALUE)
-    public byte[] getQRCode() throws WriterException {
 
-        String qrCodeText = "Name:Saugata Dutta, Age:32, Country:India";
-        int size = 250;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        ImageOutputStream ios = new MemoryCacheImageOutputStream(out);
+    @PostMapping(value = "decode", consumes = {
+            MediaType.MULTIPART_FORM_DATA_VALUE})
+    public String readQRCode(@RequestParam(required = false, value = "file") MultipartFile
+            image) throws WriterException, IOException {
 
+        File imageFile = new File(image.getOriginalFilename());
+        FileUtils.writeByteArrayToFile(imageFile,
+                image.getBytes());
+
+        BufferedImage bufferedImage = ImageIO.read(imageFile);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
         try {
-            if (!ImageIO.write(getQRImage(qrCodeText,
-                    size),
-                    "PNG",
-                    ios)) {
-                throw new IOException("ImageIO.write failed");
-            }
-            ios.close();
-        } catch (IOException ex) {
-            throw new RuntimeException("saveImage: " + ex.getMessage());
+            Result result = new MultiFormatReader().decode(bitmap);
+            return result.getText();
+        } catch (NotFoundException e) {
+            System.out.println("There is no QR code in the image");
+            return null;
         }
-        return out.toByteArray();
     }
 }
